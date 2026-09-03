f58f01a119e05d85ae703b0fafd02a6b4601a649
PedroAmadoo <Pedrohenriqueamado23@gmail.com>
2026-09-03 08:39:41 -0300

    primeiro commit

---
+++ calculadora.js
@@ -0,0 +1,18 @@
+function calcular() {
+            let num1 = Number(document.getElementById("num1").value);
+            let num2 = Number(document.getElementById("num2").value);
+            let operacao = document.getElementById("operacao").value;
+
+            let resultado;}
+
+            if (operacao == "+") {
+                resultado = num1 + num2;
+            } else if (operacao == "-") {
+                resultado = num1 - num2;
+            } else if (operacao == "*") {
+                resultado = num1 * num2;
+            } else if (operacao == "/") {
+                resultado = num1 / num2;
+            }
+
+            document.getElementById("resultado").textContent = resultado;
